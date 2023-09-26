4bb96cc9d0ce94fcc1b9bd4463a158fe31a4e705
zinh9 <127460831+zinh9@users.noreply.github.com>
2023-09-26 12:05:08 -0300

    Create Exe 16.py

---
+++ Exe 16.py
@@ -0,0 +1,13 @@
+cores_hexa = {
+    'Vermelho': '#FF0000',
+    'Verde': '#00FF00',
+    'Azul': '#0000FF',
+    'Amarelo': '#FFFF00',
+    'Ciano': '#00FFFF',
+    'Magenta': '#FF00FF',
+    'Branco': '#FFFFFF',
+    'Preto': '#000000'
+}
+
+for cor, hexa in cores_hexa.items():
+    print(f"{cor}: {hexa}")

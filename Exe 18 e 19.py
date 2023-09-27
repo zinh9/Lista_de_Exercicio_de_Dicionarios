684858151d7729aaedc9f5856aa8b459c9233cb0
zinh9 <127460831+zinh9@users.noreply.github.com>
2023-09-26 23:39:54 -0300

    Update and rename Exe 18.py to Exe 18 e 19.py

--- Exe 18 e 19.py
+++ Exe 18 e 19.py
@@ -14,3 +14,13 @@ novo_jogadores_gols = {jogador: gol for gol, jogador in gols_jogadores.items()}
 
 for jogador, gol in novo_jogadores_gols.items():
     print(f"{jogador}: {gol}")
+
+mais_gols = 0
+jogador_gol = ' '
+
+for jogador, gol in novo_jogadores_gols.items():
+    if mais_gols < gol:
+        mais_gols = gol
+        jogador_gol = jogador
+
+print(f"O jogador com mais gols é {jogador_gol}, com {mais_gols} gols!")
